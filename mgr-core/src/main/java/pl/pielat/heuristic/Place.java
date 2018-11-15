f6c6eef60687b96a5a8702ed7c8b67fd62977bf0
matt-pielat <mateusz.pielat@gmail.com>
2018-11-15 21:07:47 +0100

    Add comment explaining what Job.windowEnd is

--- mgr-core/src/main/java/pl/pielat/heuristic/Place.java
+++ mgr-core/src/main/java/pl/pielat/heuristic/Place.java
@@ -7,7 +7,7 @@ public class Place
 {
     public final Location location;
     public final double windowStart;
-    public final double windowEnd;
+    public final double windowEnd; // the latest time of arrival (NOT departure)
 
     public Place(Location location, TimeWindow timeWindow)
     {
