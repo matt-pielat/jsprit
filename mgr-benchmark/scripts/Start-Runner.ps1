15e6a9544d7eea6bded8e5c91e3260eefd09cb20
matt-pielat <mateusz.pielat@gmail.com>
2018-12-11 22:10:00 +0100

    Add powershell wrapper script of benchmark runner

---
+++ mgr-benchmark/scripts/Start-Runner.ps1
@@ -0,0 +1,22 @@
+param(
+    [string]$InputDir,
+    [bool]$TimeWindows,
+    [string]$JspritOutputDir,
+    [string]$GarridoRiffOutputDir,
+    [string]$LogDir,
+    [long]$TimeLimitInMs,
+    [int]$RunsPerProblem
+)
+
+# Example usage:
+# .\Start-Runner.ps1 -InputDir "D:\VRP Benchmarks\Set A - Augerat\Problems" -TimeWindows $false -JspritOutputDir "D:\VRP Benchmarks\Set A - Augerat\Solutions jsprit" -GarridoRiffOutputDir "D:\VRP Benchmarks\Set A - Augerat\Solutions algorithm" -LogDir "D:\VRP Benchmarks\Set A - Augerat\Logs" -TimeLimitInMs 5000 -RunsPerProblem 10
+
+$jarPath = "../output/mgr-benchmark.jar"
+
+$timeWindowsArg = ""
+if ($TimeWindows)
+{
+    $timeWindowsArg = "-tw"
+}
+
+Start-Process java -ArgumentList "-jar ""$jarPath"" -i ""$InputDir"" $timeWindowsArg -j ""$JspritOutputDir"" -gr ""$GarridoRiffOutputDir"" -l ""$LogDir"" -t $TimeLimitInMs -r $RunsPerProblem" -NoNewWindow -Wait
