00f51d7b430e3859a18c91681dd0bee23014508e
matt-pielat <mateusz.pielat@gmail.com>
2018-12-16 15:21:44 +0100

    Fix time limit setup in AlgorithmFactory

--- mgr-benchmark/src/main/java/pl/pielat/benchmark/algorithmCreation/AlgorithmFactory.java
+++ mgr-benchmark/src/main/java/pl/pielat/benchmark/algorithmCreation/AlgorithmFactory.java
@@ -2,16 +2,21 @@ package pl.pielat.benchmark.algorithmCreation;
 
 import com.graphhopper.jsprit.core.algorithm.VehicleRoutingAlgorithm;
 import com.graphhopper.jsprit.core.algorithm.termination.TimeTermination;
-import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
 import pl.pielat.algorithm.ExtendedProblemDefinition;
 
 public abstract class AlgorithmFactory
 {
     private long timeThresholdInMs = -1;
+    private int maxIterations = Integer.MAX_VALUE;
 
     public void setTimeThreshold(long timeThresholdInMs)
     {
         this.timeThresholdInMs = timeThresholdInMs;
+    }
+
+    public void setMaxIterations(int maxIterations)
+    {
+        this.maxIterations = maxIterations;
     }
 
     public VehicleRoutingAlgorithm build(ExtendedProblemDefinition vrp)
@@ -20,7 +25,11 @@ public abstract class AlgorithmFactory
 
         VehicleRoutingAlgorithm vra = createAlgorithm(vrp);
 
-        vra.setPrematureAlgorithmTermination(new TimeTermination(timeThresholdInMs));
+        TimeTermination timeTermination = new TimeTermination(timeThresholdInMs);
+        vra.setPrematureAlgorithmTermination(timeTermination);
+        vra.addListener(timeTermination);
+
+        vra.setMaxIterations(maxIterations);
 
         return vra;
     }
