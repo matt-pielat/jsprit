bd193ce577a4583066111643d4ed5a01ba22918f
matt-pielat <mateusz.pielat@gmail.com>
2019-01-12 23:31:28 +0100

    Add missing algorithm starting

--- mgr-benchmark/src/main/java/pl/pielat/benchmark/runnerEngine/BenchmarkRunner.java
+++ mgr-benchmark/src/main/java/pl/pielat/benchmark/runnerEngine/BenchmarkRunner.java
@@ -165,6 +165,8 @@ public class BenchmarkRunner
                                 runProcessor.processRun(args);
                             }
                         });
+
+                        vra.searchSolutions();
                     }
                     catch (Exception e)
                     {
