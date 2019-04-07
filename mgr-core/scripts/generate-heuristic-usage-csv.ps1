6450d2f8e54ddeb6cba922f740fbe36976c3e9a4
matt-pielat <mateusz.pielat@gmail.com>
2019-04-07 12:52:41 +0200

    Add script for generating heuristic usage csv

---
+++ mgr-core/scripts/generate-heuristic-usage-csv.ps1
@@ -0,0 +1,43 @@
+$dataRoot = "D:\Google Drive\Magisterka\data\Convergence"
+$problemIds = Get-ChildItem "${dataRoot}\Problems" -File | Select-Object -ExpandProperty BaseName
+$solutionDir = Get-ChildItem "${dataRoot}\Solutions\GarridoRiff" -File
+
+$orderingHeuristicIds = @()
+$constructiveHeuristicIds = @()
+$repairingHeuristicIds = @()
+
+$data = @()
+
+foreach ($problemId in $problemIds) {
+    $solutionFiles = Get-ChildItem $solutionDir.FullName | Where-Object { $_.BaseName.StartsWith($problemId) } | Sort-Object
+        
+    foreach ($solutionFile in $solutionFiles) {
+        $xmlDoc = New-Object System.Xml.XmlDocument
+        $xmlDoc.Load($solutionFile.FullName)
+
+        $orderingHeuristicIds += ($xmlDoc.solution.heuristicUsages.orderingHeuristicUsages.hu | Select-Object -ExpandProperty id)
+        $constructiveHeuristicIds += ($xmlDoc.solution.heuristicUsages.constructiveHeuristicUsages.hu | Select-Object -ExpandProperty id)
+        $repairingHeuristicIds += ($xmlDoc.solution.heuristicUsages.repairingHeuristicUsages.hu | Select-Object -ExpandProperty id)
+
+        $heuristicUsages = $xmlDoc.solution.heuristicUsages.orderingHeuristicUsages.hu + 
+            $xmlDoc.solution.heuristicUsages.constructiveHeuristicUsages.hu +
+            $xmlDoc.solution.heuristicUsages.repairingHeuristicUsages.hu | 
+            Select-Object id, @{Name = 'usage count'; Expression = '#text' }
+
+        $dataItem = @{Id = "$($solutionFile.BaseName)" }
+        foreach ($hu in $heuristicUsages) {
+            $propertyName = $hu.id
+            $propertyValue = [int]::Parse($hu.'usage count')
+            $dataItem.$propertyName = $propertyValue
+        }
+        $data += [PSCustomObject]$dataItem
+    }
+}
+
+$orderingHeuristicIds = $orderingHeuristicIds | Sort-Object -Unique
+$constructiveHeuristicIds = $constructiveHeuristicIds | Sort-Object -Unique
+$repairingHeuristicIds = $repairingHeuristicIds | Sort-Object -Unique
+
+$csvPath = "D:\heuristic_usage.tsv"
+$keys = , "Id" + $orderingHeuristicIds + $constructiveHeuristicIds + $repairingHeuristicIds
+$data | Select-Object $keys | Export-Csv -Delimiter "`t" -Path $csvPath -NoTypeInformation 
